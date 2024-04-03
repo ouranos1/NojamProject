2f91c9d2c207ef260fa123e5775bc219881341bc
ouranos1 <ouranos0@naver.com>
2024-04-03 14:56:46 +0900

    api테스트중

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 // import './App.css';
-
+import './test.css';
+import axios from 'axios';
 import { Component } from "react";
 
-class Subject extends Component{
-  render(){
+class Subject extends Component {
+  render() {
     return (
       <header>
-        <h1>
-          TEST
+        {/* /*id="project_title"* */}
+        <h1> 
+          
         </h1>
         테스트중입니다.
       </header>
@@ -15,19 +17,55 @@ class Subject extends Component{
   }
 }
 
-function App() {
-  return (
-    <div className="home">
-      <Subject></Subject>
-      <user_page>
-        <h2>닉네임, 마이페이지</h2>
-      </user_page>
+class Login_List extends Component{
+  render() {
+    return(
+      <div id="login_List">
+        <div id="id_Input_Layer">
+          <h5>ID</h5>
+          <input id="id_Input" type="text"></input>
+        </div>
+        <div className="passWord_Input_Layer">
+          <h5>PW</h5>
+          <input id="passWord_Input" type="text"></input>
+        </div>
+      </div>
+    );
+  }
+}
 
-      <header>
-        <h1>게임 파티 모집</h1>
-      </header>
+function App () {
+  
+  const login_Event = async () => {
+    console.log("테수트");
+    try{
+      const response = await axios.get("/user/1");
+      console.log(response.data);
+    }
+    catch(error){
+      console.log("오류", error);
+    }
+  }
+
+  return (
+    <div className="Login_Layer">
+      {/* <Subject></Subject> */}
+      <h2>로그인 화면</h2>
+      <div className="Title">
+        <h2>타이틀</h2>
+      </div>
+     <Login_List></Login_List>
+     <div className="button_Layer">
+          <button type="submit" id="login_Button" onClick={login_Event}>로그인</button><br></br>
+          <button type="submit" id="signUp" onClick={signUp_Event}>회원가입</button>
+        </div>
     </div>
   );
+}
+
+function signUp_Event(){
+  console.log("테스트");
+
 }
 
 export default App;
---
+++ src/setupProxy.js
@@ -0,0 +1,11 @@
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+module.exports = function(app) {
+  app.use(
+    "/user",
+    createProxyMiddleware({
+      target: "http://ec2-3-35-139-15.ap-northeast-2.compute.amazonaws.com:8080/api",
+      changeOrigin: true,
+    })
+  );
+};
